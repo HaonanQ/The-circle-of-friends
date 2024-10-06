d95f1c4aa81618b324e144f0c7573e54c7460df9
北鱼 <13287774+zhuifengbeiyu@user.noreply.gitee.com>
2024-10-06 17:08:26 +0800

    添加购物车

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopCartController.java
@@ -0,0 +1,30 @@
+package com.sky.controller.user;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/shoppingCart")
+@Slf4j
+@Api(tags = "用户购物车")
+public class ShopCartController {
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @PostMapping("/add")
+    @ApiOperation(value = "添加商品到购物车", notes = "添加商品到购物车")
+    public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        log.info("添加商品到购物车：{}",shoppingCartDTO);
+        shoppingCartService.ShoppingCardadd(shoppingCartDTO);
+        return Result.success();
+    }
+}
