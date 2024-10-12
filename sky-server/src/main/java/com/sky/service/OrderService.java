76766a8a298e5ed8d9c095bf3ec2361cf9617bc6
北鱼 <13287774+zhuifengbeiyu@user.noreply.gitee.com>
2024-10-12 20:02:02 +0800

    商家接单

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -30,4 +30,6 @@ public interface OrderService {
     PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO);
 
     OrderStatisticsVO statistics();
+
+    void confirm(OrdersConfirmDTO ordersConfirmDTO);
 }
