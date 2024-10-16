e43b667b1824487452e69629ecd7dbcce066d450
北鱼 <13287774+zhuifengbeiyu@user.noreply.gitee.com>
2024-10-16 16:25:08 +0800

    导出数据

--- sky-server/src/main/java/com/sky/controller/admin/RrportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/RrportController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 
@@ -52,5 +53,11 @@ public class RrportController {
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
         return Result.success(reportService.getSalesTop10(begin,end));
+    }
+
+    @GetMapping("/export")
+    @ApiOperation("报表数据")
+    public void report(HttpServletResponse response){
+        reportService.export(response);
     }
 }
